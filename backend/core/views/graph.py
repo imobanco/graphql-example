7cb72dcde2ba90d0dfb47df0a1868f3e51eaa946
rodrigondec <rodrigondec@gmail.com>
2022-08-27 14:26:12 -0300

    adiciona novas queries

--- backend/core/views/graph.py
+++ backend/core/views/graph.py
@@ -1,8 +1,9 @@
 import graphene
 
 from person.views.graph import PersonQuery
+from todo.views.graph import TodoQuery, TodoItemQuery
 
-queries = (PersonQuery,)
+queries = (PersonQuery, TodoQuery, TodoItemQuery)
 
 
 Query = type("Query", queries, {})
