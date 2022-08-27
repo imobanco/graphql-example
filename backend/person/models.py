b11ccc773d536bdaeec60cdef6ea5a138caf4ac1
rodrigondec <rodrigondec@gmail.com>
2022-08-27 10:56:27 -0300

    adiciona app person

---
+++ backend/person/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from core.models import BaseModel
+
+
+class Person(BaseModel):
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "Pessoa"
+        verbose_name_plural = "Pessoas"
+
+    name = models.CharField(verbose_name="nome", help_text="nome completo")
