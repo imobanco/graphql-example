7c7aaf8e7a21db66a8408b43a25b7544887d44a0
rodrigondec <rodrigondec@gmail.com>
2022-08-27 13:38:06 -0300

    schema geral do graphql

---
+++ backend/core/views/graph.py
@@ -0,0 +1,14 @@
+import graphene
+
+from person.views.graph import PersonQuery
+
+
+queries = (
+    PersonQuery,
+)
+
+
+Query = type('Query', queries, {})
+
+
+schema = graphene.Schema(query=Query)
