a35b382d13f14f352947de17ac38b67d921ddecc
rodrigondec <rodrigondec@gmail.com>
2022-08-28 17:33:32 -0300

    adiciona chamadas do PersonRepository no HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
+import {BakcendHttpAdaptor} from "@/core/adapters/http_backend"
+import {PersonRepository} from "@/person/repository"
+
+const myCall = async () => {
+  console.log('my call')
+  const repo = new PersonRepository()
+  let person = await repo.create('Meu Nome teste')
+  console.log('create', person)
+  person.name = 'Meu nome alterado de teste'
+  person = await repo.update_entity(person)
+  console.log('update entity', person)
+  person = await repo.update_data(person.id, {'name': 'Meu nome alterado sem entity de teste'})
+  console.log('update entity', person)
+}
+
 </script>
 
 <template>
   <main>
+    <button @click='myCall'>Me aperte</button>
     <TheWelcome />
   </main>
 </template>
