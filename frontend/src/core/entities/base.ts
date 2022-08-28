c2ace7e936efd1508928a22d8e55034269021d0f
rodrigondec <rodrigondec@gmail.com>
2022-08-28 17:32:57 -0300

    converte BaseModel e Person para classes

--- frontend/src/core/entities/base.ts
+++ frontend/src/core/entities/base.ts
@@ -1,3 +1,13 @@
+export class BaseModelc{
+    constructor(
+        public readonly id: string,
+        public readonly created_at: Date,
+        public readonly updated_at: Date,
+    ){}
+}
+
+
+
 export type BaseModel = {
     id: string
     created_at: string
--- frontend/src/person/entities/person.ts
+++ frontend/src/person/entities/person.ts
@@ -1,6 +1,30 @@
-import type {BaseModel} from '@/core/entities'
+import {BaseModelc} from '@/core/entities'
 
+export class Person extends BaseModelc{
+    constructor(
+        public name: string,
+        id: string,
+        created_at: Date,
+        updated_at: Date,
+    ){
+        super(id, created_at, updated_at)
+    }
 
-export type Person = BaseModel & {
-    name: String
+    to_object(){
+        return {
+            'id': this.id,
+            'created_at': this.created_at,
+            'updated_at': this.updated_at,
+            'name': this.name,
+        }
+    }
+
+    static from_data(person_data: any){
+        return new Person(
+            person_data.name, 
+            person_data.id, 
+            person_data.created_at, 
+            person_data.updated_at
+        )
+    }
 }
